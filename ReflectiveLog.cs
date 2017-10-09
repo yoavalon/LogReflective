5e1b7a87ccf5f9ce570758cd646c8d686f6bbd83
Yoav Alon <31308918+yoavalon@users.noreply.github.com>
2017-10-09 13:11:57 +0300

    Add files via upload

---
+++ ReflectiveLog.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RefInTest
+{
+    class Calculations
+    {
+        public void Divide(int a, int b)
+        {
+            try
+            {
+                Console.WriteLine(a / b);                
+            }
+            catch (Exception ex)
+            {
+                Log(ex);
+            }
+        }
+
+        public static void Log(Exception ex)
+        {
+            try
+            {
+                string TimeStamp = $"{DateTime.Now.ToShortDateString()}-{DateTime.Now.ToLongTimeString()}:";
+                var Msg = $"{TimeStamp} {ex?.TargetSite.ReflectedType.Name}-{ex?.TargetSite.Name}(): {ex?.Message} {ex?.InnerException?.Message}";
+
+                string m_LocalFolder = "D:\\Logs\\";  //Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + "\\FMR\\PackageSurveillance\\Logs\\";
+
+                string LogPath = m_LocalFolder + "Log" + String.Format("{0:yyyyMMdd}", DateTime.Today) + ".txt";
+                Directory.CreateDirectory(m_LocalFolder);
+
+                using (StreamWriter w = File.AppendText(LogPath))
+                {
+                    w.WriteLine(Msg);
+                }
+            }
+            catch { }
+        }
+
+    }
+}
